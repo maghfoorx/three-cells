f3a4c3cadd13599b131a724b89b3ee5a4d79b0f9
Maghfoor <maghfoor.dev@gmail.com>
2025-11-10 22:50:41 +0000

    added button click events

--- apps/website/app/root.tsx
+++ apps/website/app/root.tsx
@@ -45,7 +45,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <script
           defer
           data-domain="three-cells.com"
-          src="https://p.maghfoor.com/js/script.js"
+          src="https://p.maghfoor.com/js/script.outbound-links.pageview-props.tagged-events.js"
         />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
