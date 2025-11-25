a9e30e10d703a9d1a03a8a5821c48d4497cad585
Maghfoor <maghfoor.dev@gmail.com>
2025-11-25 17:56:07 +0000

    added lower sized images

--- apps/website/app/components/AppLogoIcon.tsx
+++ apps/website/app/components/AppLogoIcon.tsx
@@ -4,7 +4,6 @@ export default function AppLogoIcon({ className }: { className: string }) {
   return (
     <div>
       <img
-        loading="lazy"
         src="/meditating.webp"
         alt="Three Cells application logo"
         className={cn("aspect-square", className)}
--- apps/website/app/root.tsx
+++ apps/website/app/root.tsx
@@ -59,7 +59,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
           media="print"
-          onLoad="this.media='all'"
         />
 
         {/* Static Meta Tags for SEO and Social */}
